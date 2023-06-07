58f115c395375be4605988c2b92356053480ce94
actyvystom <thomas.goerldt@actyvyst.com>
2023-06-07 17:08:56 +0200

    add backend-create solution

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -7,4 +7,12 @@ export default async function handler(request, response) {
     const products = await Product.find({});
     return response.status(200).json(products);
   }
+  if (request.method === "POST") {
+    try {
+      const product = await Product.create(request.body);
+      return response.status(201).json(product);
+    } catch (error) {
+      return response.status(500).json({ message: error.message });
+    }
+  }
 }
